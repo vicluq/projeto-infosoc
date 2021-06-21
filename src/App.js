df4430ffa578808f9df38542504a49862aaaef4b
Victoria Luquet <victoria.tewari@gmail.com>
2021-06-21 16:27:00 -0300

    finished (okay, now)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback } from "react";
 import Header from "./Components/Header";
 import Form from "./Screens/FormScreen";
 import Showcase from "./Screens/DisplayScreen";
-import Loading from './Components/Loading'
+import ThankYou from "./Components/ThankYouView";
+import Loading from "./Components/Loading";
 import ScreensType from "./data/screens";
 import "./App.css";
 
@@ -10,22 +11,26 @@ function App() {
   const [screen, setScreen] = useState(ScreensType.FORM);
   const [loading, setLoading] = useState(false);
 
-  const changeScreen = (screen) => {
+  const changeScreen = useCallback((screen) => {
     setLoading(true);
     setTimeout(() => {
-      // Kill loading screen
       setScreen(screen);
       setLoading(false);
     }, 1200);
-  };
+  }, []);
 
   return (
     <div className="App">
       {loading && <Loading />}
       <Header screen={screen} changeScreen={changeScreen} />
       <div className="App-Content">
-        {screen === ScreensType.FORM && <Form changeScreen={changeScreen} setLoading={setLoading} />}
-        {screen === ScreensType.SHOWCASE && <Showcase setLoading={setLoading} />}
+        {screen === ScreensType.FORM && (
+          <Form changeScreen={changeScreen} setLoading={setLoading} />
+        )}
+        {screen === ScreensType.SHOWCASE && (
+          <Showcase setLoading={setLoading} />
+        )}
+        {screen === ScreensType.THANK_YOU_SCREEN && <ThankYou />}
       </div>
     </div>
   );
---
+++ src/Components/ThankYouView/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const ThankYou = (props) => {
+    return (
+        <Container>
+            <h1>Obrigada pela contribuição! 😁</h1>
+        </Container>
+    )
+}
+
+export default ThankYou;
